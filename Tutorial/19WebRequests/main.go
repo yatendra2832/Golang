3c63ada057d12c0dfdda3e8db242c3d1148b6eeb
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-04-20 22:59:07 +0530

    handling web request in the golang

---
+++ Tutorial/19WebRequests/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://localhost5000.com"
+
+func main() {
+	fmt.Println("WebRequest in Golang")
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Response is of type: %T\n", response)
+
+	defer response.Body.Close()
+
+	databytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	content := string(databytes)
+	fmt.Println(content)
+
+}
