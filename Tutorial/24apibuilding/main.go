2c2861586cb24fbd6cd4c033db4c6fc57fe9baca
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-04-24 00:05:07 +0530

    sending a API response in JSON

--- Tutorial/24apibuilding/main.go
+++ Tutorial/24apibuilding/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Model for course - file
 type Course struct {
@@ -23,4 +30,21 @@ func (c *Course) isEmpty() bool {
 }
 func main() {
 	fmt.Println("Building APIs ...")
+	r := mux.NewRouter()
+	r.HandleFunc("/", serverHome).Methods("GET")
+	log.Fatal(http.ListenAndServe(":3000", r))
+
+}
+
+// controllers
+//
+//	server home route
+func serverHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome To APIs by Yatendra Singh Learning Golang</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter,r *http.Request){
+	fmt.Println("Get All Courses")
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(courses)
 }
